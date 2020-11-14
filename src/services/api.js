ca232ddd7b3f3ebbdc7908d69bccfceb8d8b5af8
Murilo Ubuntu <murilo@amo.delivery>
2020-11-14 12:16:08 -0300

    wip

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://pilas-node.herokuapp.com'
+  baseURL: 'http://localhost:8080/'
+  // baseURL: 'https://pilas-node.herokuapp.com'
 })
 
 export default api
